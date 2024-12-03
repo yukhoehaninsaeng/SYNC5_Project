f326a7a909827ab2c6682c46d697e0045b0aa982
Yukhoeinsaeng <kim12312320@gmail.com>
2024-12-03 18:20:27 +0900

    update ver1.5

--- purchaseorderinquiry01/webapp/controller/purchaseorderinquiry_v.controller.js
+++ purchaseorderinquiry01/webapp/controller/purchaseorderinquiry_v.controller.js
@@ -5,8 +5,9 @@ sap.ui.define([
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
     "sap/ui/core/syncStyleClass",
-	"sap/m/library"
-], function (Controller, MessageToast, Fragment, Filter, FilterOperator, syncStyleClass, library) {
+	"sap/m/library",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, Fragment, Filter, FilterOperator, syncStyleClass, library, JSONModel) {
     "use strict";
 
     const SelectDialogInitialFocus = library.SelectDialogInitialFocus;
@@ -36,6 +37,11 @@ sap.ui.define([
                         });
 
             oView.getModel("statusCounts").setProperty("/PurchaseOrder", oStatusCounts);   //계산된 값을 statusCounts 모델에 업데이트
+
+            var oViewModel = new JSONModel({
+                isVisible: true // 초기값: true (All 탭)
+            });
+            this.getView().setModel(oViewModel, "viewModel");
         },
         error: (oError) => {
             console.error("PurchaseOrder 데이터 로드 오류:", oError);
@@ -51,53 +57,98 @@ sap.ui.define([
                 oTable   = this.byId("PurchaseOrder"),
                 oBinding = oTable.getBinding("rows"),
 				// Array to combine filters
-				aFilters = [];
+				aFilters = [],
+                oViewModel = this.getView().getModel("viewModel");
 
                 switch (sKey) {
                     case "A": // "입고 완료" 필터
                         aFilters.push(new Filter("stostat_txt", FilterOperator.EQ, "입고 완료"));
+                        oViewModel.setProperty("/isVisible", false);
                         break;
                     case "B": // "입고 예정" 필터
                         aFilters.push(new Filter("stostat_txt", FilterOperator.EQ, "입고 예정"));
+                        oViewModel.setProperty("/isVisible", false);
                         break;
                     default: // 모든 데이터 표시
                         aFilters = [];
+                        oViewModel.setProperty("/isVisible", true);
                 }
                 
 			oBinding.filter(aFilters);
 		},
 
-
-        onOpenDialog : function(oEvent){
+        onOpenDialog: function (oEvent) {
             var oButton = oEvent.getSource();
             var oContext = oButton.getParent().getBindingContext();
-            var vAufnr = oContext.getProperty('aufnr');
-          
+            var vAufnr = oContext.getProperty("aufnr");
+        
             console.log(oContext);
-
-            if(!this.pDialog){
-                this.pDialog = this.loadFragment({
-                    name : "cl3.syncyoung.mm.purchaseorderinquiry.purchaseorderinquiry.view.Dialog"
-                });
+        
+            if (!this.pDialog) {
+                // Fragment를 동적으로 생성
+                this.pDialog = sap.ui.xmlfragment(
+                    "cl3syncyoungpi.purchaseorderinquiry01.view.Dialog",
+                    this // 컨트롤러를 Fragment의 이벤트 핸들러 컨텍스트로 설정
+                );
+        
+                // Dialog를 현재 뷰에 추가
+                this.getView().addDependent(this.pDialog);
             }
-            
-            this.pDialog.then(function(oDialog){
-                var oTable = oDialog.getContent()[0];       // 다이얼로그 첫 번째 자식이 테이블이어야 함
-
+        
+            // Fragment가 생성된 이후 실행
+            if (this.pDialog) {
+                var oTable = this.pDialog.getContent()[0]; // 다이얼로그 첫 번째 자식이 테이블이어야 함
+        
                 if (oTable && oTable.isA("sap.ui.table.Table")) {
                     var oBinding = oTable.getBinding("rows");
-
+        
                     // 필터 설정
-                    var aFilter = [ new Filter("aufnr", FilterOperator.EQ, vAufnr )]
+                    var aFilter = [new sap.ui.model.Filter("aufnr", sap.ui.model.FilterOperator.EQ, vAufnr)];
                     oBinding.filter(aFilter);
                 }
+        
+                this.pDialog.open();
+            }
+        },
 
-                oDialog.open();
-            });
-            
-      },
-       onCloseDialog : function ( ){
-        this.byId("itemDialog").close();
-       }
+        onCloseDialog: function () {
+            if (this.pDialog) {
+                this.pDialog.close();
+            }
+        },
+
+        onFilter: function(oEvent) {
+			this.sSearchQuery = oEvent.getSource().getValue();
+			this.fnApplyFiltersAndOrdering();
+		},
+
+        fnApplyFiltersAndOrdering: function(oEvent) {
+			var aFilters = [];
+
+			if (this.sSearchQuery) {
+				var oFilter = new Filter("aufnr", FilterOperator.Contains, this.sSearchQuery);
+				aFilters.push(oFilter);
+			}
+
+			this.byId("PurchaseOrder").getBinding("rows").filter(aFilters);
+		},
+
+        onReset: function () {
+            // SearchField 초기화
+            var oSearchField = this.byId("searchPO");
+            if (oSearchField) {
+                oSearchField.setValue("");
+            }
+        
+            // 테이블의 필터 초기화
+            var oTable = this.byId("PurchaseOrder");
+            var oBinding = oTable.getBinding("rows");
+            if (oBinding) {
+                oBinding.filter([]); // 필터 제거
+            }
+        
+            // 필요한 경우 상태값 초기화
+            this.sSearchQuery = null; // 내부 검색 상태 리셋
+        }
     });
 });
